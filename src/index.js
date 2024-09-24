2d9c6a37142c3a839cd7db2d31d33739fd19e460
Sfincs2023 <Naumenko.Aleksey@gmail.com>
2024-09-24 19:19:31 -0400

    reverse-int fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 module.exports = function reverse (n) {
     let strNum = n.toString();
     let reverseStr = strNum.split('').reverse().join('');
-    return reverseStr;
+    let reverseNum = parseInt(reverseStr, 10);
+    if (n < 0) {
+        reverseNum *= -1;
+    }
+    return reverseNum;
 }
